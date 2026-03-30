cc789f809ae7c3bc33be15f7b7d735bf348ad7fc
teodurieux <teopaj1@gmail.com>
2026-03-30 21:30:30 +0200

    Delete database.js

--- database.js
+++
@@ -1,7 +0,0 @@
-const { DatabaseSync } = require('node:sqlite');
-const path = require('path');
-
-const dbPath = path.join(__dirname, 'cashcash.db');
-const db = new DatabaseSync(dbPath);
-
-module.exports = db;
